1d3840f3290e11ca038faf5cd0bfbac74f1b142e
Kefa Mutuma <mutmakeffa@gmail.com>
2019-08-30 00:47:50 +0300

    style changes index page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,11 @@
 //All custom Javascripts
 
-$(".card-hover").click(function() {
-    window.location = $(this).find("a").attr("href"); 
-    return false;
-  });
+// $(".card-hover").click(function() {
+//     window.location = $(this).find("a").attr("href"); 
+//     return false;
+//   });
 
-  $(".card-clickable").click(function() {
+$(".card-clickable").click(function() {
     window.location = $(this).find("a").attr("href"); 
     return false;
   });
